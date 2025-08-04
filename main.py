5dff0e2f3fd94099b0741259f74556c714abce89
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-08-05 05:22:48 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import cv2
-import cvzone
-from ultralytics import YOLO
+import  mediapipe as mp
+import time
 
-model = YOLO('./Yolo-Weights/yolov8m.pt').to('cuda')
 cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     ret,frame = cap.read()
-    result = model(frame)
-    annotated_frame = result[0].plot()
-    cv2.imshow('YOLO',annotated_frame)
 
+    imageRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+
+    if results.multi_hand_landmarks :
+        for handlms in results.multi_hand_landmarks:
+            custom_connection_style = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2)
+            mpDraw.draw_landmarks(frame,handlms,mpHands.HAND_CONNECTIONS,connection_drawing_spec=custom_connection_style)
+
+
+    cv2.imshow('Hand Tracking',frame)
     if cv2.waitKey(1) == ord('x'):
         break
 cv2.destroyAllWindows()
