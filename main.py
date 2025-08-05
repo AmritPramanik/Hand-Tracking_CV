5f05d702f2363f7fc5f917573978e67ee3a74507
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-08-06 01:36:08 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import  mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     ret,frame = cap.read()
@@ -15,9 +21,25 @@ while True:
 
     if results.multi_hand_landmarks :
         for handlms in results.multi_hand_landmarks:
-            custom_connection_style = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2)
+            for id,lm in enumerate(handlms.landmark):
+                h,w,c = frame.shape
+                cx,cy = int(lm.x*w),int(lm.y*h)
+                print(id,cx,cy)
+                if id==4 :
+                    cv2.circle(frame, (cx, cy),15,(255,0,255),thickness=-1)
+
+                print(id, cx, cy)
+                if id == 8:
+                    cv2.circle(frame, (cx, cy), 15, (255,0,0), thickness=-1)
+
+            custom_connection_style = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=4)
             mpDraw.draw_landmarks(frame,handlms,mpHands.HAND_CONNECTIONS,connection_drawing_spec=custom_connection_style)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame,str(f"FPS : {int(fps)}"),(10,30),cv2.FONT_HERSHEY_DUPLEX,1,(0,255,0),2)
 
     cv2.imshow('Hand Tracking',frame)
     if cv2.waitKey(1) == ord('x'):
